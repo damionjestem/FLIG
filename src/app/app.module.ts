9df450b9f237bf6281302f3e0d8a5afe62f39bb7
Damian Lesiewicz <ndnd@protonmail.ch>
2021-11-13 13:00:53 +0100

    added FlickrService & FormBuilder to providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -9,6 +9,7 @@ import { MenubarComponent } from './menubar/menubar.component';
 import { MaincontainerComponent } from './maincontainer/maincontainer.component';
 import { FormComponent } from './form/form.component';
 import { PreviewComponent } from './preview/preview.component';
+import { FlickrService } from './flickr.service';
 
 @NgModule({
   declarations: [
@@ -22,9 +23,13 @@ import { PreviewComponent } from './preview/preview.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    FlickrService,
+    FormBuilder
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
